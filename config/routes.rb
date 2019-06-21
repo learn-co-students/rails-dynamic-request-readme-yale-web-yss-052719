96e3dbcfed83dd018abde15f976872f01d8805fa
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-21 11:53:12 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: :show
+
+  #dynamic routes can receive a parameter (:id) and passes the parameter to the controller's show action
 end
